0447730f7c4a20c5ecb391920fcb267add927ed8
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-01-04 17:03:50 +0100

    Create node server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(`URL : ${req.url}`);
+    console.log(`METHOD : ${req.method}`);
+    
+    
+});
+
+server.listen(8080, () => {
+    console.log('Server started on Port 8080 !');
+});
